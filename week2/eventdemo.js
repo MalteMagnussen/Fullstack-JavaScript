f0f61305a9dd827ec121bad370e0c0bc6d39487a
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-05 11:25:30 +0100

    Update eventdemo.js

--- week2/eventdemo.js
+++ week2/eventdemo.js
@@ -1,13 +1,29 @@
 const EventEmitter = require("events");
 
+const consoleInput = process.argv.slice(2).map(element => Number(element));
+
 class MyEventPublisher extends EventEmitter {
   __numbers = [];
 
   addNumber(number) {
     this.__numbers.push(number);
+    if (number % 2 === 0) {
+      this.emit("even", { number });
+    } else if (number % 2 != 0) {
+      this.emit("odd", { number });
+    }
+    if (number > 100) {
+      this.emit("high", { number });
+    } else {
+      this.emit("low", { number });
+    }
   }
 }
 
 const publisher = new MyEventPublisher();
+publisher.on("odd", number => console.log(`${number.number} is odd`));
+publisher.on("even", number => console.log(`${number.number} is even`));
+publisher.on("high", number => console.log(`${number.number} is high`));
+publisher.on("low", number => console.log(`${number.number} is low`));
 
-publisher.addNumber(10);
+consoleInput.forEach(element => publisher.addNumber(element));
