6475f35acab75d1fbe50b9badf4c8ab9baf2af5d
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-05 09:21:33 +0100

    Fire Event and Comments

--- week2/dosDetector.js
+++ week2/dosDetector.js
@@ -4,20 +4,33 @@
 // fire (emit) an event "DosDetected" if the same URL is added more
 // than once before the time-interval TIME_BETWEEN_CALLS has expired.
 class DOS_Detector {
+  // timeValue = Minimum acceptable time between calls.
   constructor(timeValue) {
     super();
+    // Map of key: url, value: time when last call was made.
     this.urls = new Map();
+    // Acceptable time between calls from one url.
     this.TIME_BETWEEN_CALLS = timeValue;
   }
+  // Method to call when a new url requests site.
   addUrl = url => {
+    // Get current time
     const time = new Date().getTime();
+    // If URL exists in map already.
     if (this.urls.has(url)) {
+      // Take current time and substract the time when last call from that URL was made.
       const deltaTime = time - this.urls.get(url);
+      // If that delta time is less than the acceptable time between calls, then fire DosDetected event.
       if (deltaTime < this.TIME_BETWEEN_CALLS) {
         console.log("TODO: Fire the 'DosDetected' event");
         //Add this info to the event {url:url,timeBetweenCalls:deltaTime}
+        // Import emitter.
+        const emitter = require("events");
+        // Fire event
+        emitter.emit("DosDetected", { url: url, timeBetweenCalls: deltaTime });
       }
     }
+    // Add url to map with current time.
     this.urls.set(url, time);
   };
 }
