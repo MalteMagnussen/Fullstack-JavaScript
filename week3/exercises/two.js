83099b0af982431473938ebdc385389777761652
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-12 10:49:08 +0100

    :christmas_tree:

--- week3/exercises/two.js
+++ week3/exercises/two.js
@@ -19,7 +19,39 @@ const fetch = require("node-fetch");
 //            "https://swapi.co/api/films/1/",
 //            "https://swapi.co/api/films/7/"],
 
-const getPlanetforFirstSpeciesInFirstMovieForPerson = async id => {
+//Plain promises
+const getPlanetforFirstSpeciesInFirstMovieForPerson = id => {
+  fetch("https://swapi.co/api/people/" + id)
+    .then(res => res.json())
+    .then(data => {
+      console.log("Name:", data.name);
+      fetch(data.films[0])
+        .then(res => res.json())
+        .then(firstFilm => {
+          console.log("First film:", firstFilm.title);
+          fetch(firstFilm.species[0])
+            .then(res => res.json())
+            .then(species => {
+              console.log("First species:", species.name);
+              fetch(species.homeworld)
+                .then(res => res.json())
+                .then(homeworld =>
+                  console.log("Homeworld for Specie:", homeworld.name)
+                )
+                .catch(err => console.log("Error: ", err))
+                .finally(() => console.log("Done 4"));
+            })
+            .catch(err => console.log("Error: ", err))
+            .finally(() => console.log("Done 3"));
+        })
+        .catch(err => console.log("Error: ", err))
+        .finally(() => console.log("Done 2"));
+    })
+    .catch(err => console.log("Error: ", err))
+    .finally(() => console.log("Done 1"));
+};
+
+const getPlanetforFirstSpeciesInFirstMovieForPersonAsync = async id => {
   try {
     const data = await fetch("https://swapi.co/api/people/" + id).then(res =>
       res.json()
