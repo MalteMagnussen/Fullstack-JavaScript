91bf99faabaad246d7f5ae6aa338a05b11a36b52
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-19 10:46:28 +0100

    Moving on to output management

--- week4/webpack-demo/webpack.config.js
+++ week4/webpack-demo/webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
       }
     ]
   }
