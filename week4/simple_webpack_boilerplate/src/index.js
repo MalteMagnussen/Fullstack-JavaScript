89275f1675acbbc72b48caea6f4e2e15772d974a
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-19 18:21:46 +0100

    retry

--- week4/simple_webpack_boilerplate/src/index.js
+++ week4/simple_webpack_boilerplate/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 const Index = () => {
   const name = "Kurt Wonnegut";
   setTimeout(() => alert(`Hello there from ${name}`), 1000);
-
   return <div>Hello React!</div>;
 };
 
--- week4/simple_webpack_boilerplate/webpack.config.js
+++ week4/simple_webpack_boilerplate/webpack.config.js
@@ -10,12 +10,9 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
+          loader: "babel-loader"
         }
       },
       {
