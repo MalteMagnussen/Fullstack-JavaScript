bddac8039eb73e92a530f650fd698388986ab567
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-25 18:02:37 +0100

    Strong types. Fixed LocaleCompare spelling error.

--- week5/hejlsberg/main.ts
+++ week5/hejlsberg/main.ts
@@ -1,7 +1,12 @@
-function sortByName(a) {
+interface Person {
+  name: string;
+  age: number;
+}
+
+function sortByName(a: Person[]) {
   var result = a.slice(0);
   result.sort(function(x, y) {
-    return x.name.localCompare(y.name);
+    return x.name.localeCompare(y.name);
   });
   return result;
 }
