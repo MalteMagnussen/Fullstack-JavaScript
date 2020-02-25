640720b1db6efe77d8a7db0ae487008114c1bb46
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-25 18:11:21 +0100

    quick intro to typescript done

--- week5/hejlsberg/main.ts
+++ week5/hejlsberg/main.ts
@@ -3,6 +3,13 @@ interface Person {
   age: number;
 }
 
+class Greeter {
+  name: string;
+  sayHello() {
+    console.log("Hello" + name);
+  }
+}
+
 function sortByName(a: Person[]) {
   var result = a.slice(0);
   result.sort((x, y) => {
