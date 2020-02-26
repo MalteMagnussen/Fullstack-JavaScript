dae7b0d07134eab8fdc43df010dc0d2c1dffda5e
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-26 10:30:24 +0100

    typescript types

---
+++ week5/Typescript/build/logger.js
@@ -0,0 +1,8 @@
+"use strict";
+const logger = (a, b) => {
+    console.log(`Value 1: ${a}, Value 2: ${b}`);
+};
+let a = 12;
+let b = "Hello World";
+logger(a, b);
+//# sourceMappingURL=logger.js.map
---
+++ week5/Typescript/src/logger.ts
@@ -0,0 +1,8 @@
+const logger = (a: number, b: string) => {
+  console.log(`Value 1: ${a}, Value 2: ${b}`);
+};
+
+let a = 12;
+let b = "Hello World";
+
+logger(a, b);
