71bb3b719da46643a63cdda6485c17061e440b1f
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-26 11:26:07 +0100

    generics

--- week5/Typescript/build/logger.js
+++ week5/Typescript/build/logger.js
@@ -4,11 +4,39 @@ const logger = (a, b) => {
 };
 let a = 12;
 let b = "Hello World";
-logger(a, b);
 const loggerV2 = (a, b) => {
     console.log(`Name of Person: ${JSON.stringify(a)}, Address: ${b.address}`);
 };
 const person = { name: "Kurt" };
 const address = { address: "123 Main St" };
-loggerV2(person, address);
+// loggerV2(person, address);
+class Person {
+    constructor(_name) {
+        this._name = _name;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(val) {
+        this._name = val;
+    }
+}
+let p = new Person("Kurt Wonnegut");
+p.name = "Hanne Wonnegut";
+// loggerV2(p, address);
+const loggerV3 = (a, b) => {
+    console.log(`Person: ${JSON.stringify(a)}, 
+  Address: ${JSON.stringify(b)}`);
+};
+// loggerV3(a, b);
+class GenericLogger {
+    constructor() {
+        this.log = (a, b) => console.log(`Val1: ${JSON.stringify(a)}, 
+  Val2: ${JSON.stringify(b)}`);
+    }
+}
+const logger1 = new GenericLogger();
+const logger2 = new GenericLogger();
+logger1.log(a, b);
+logger2.log(person, address);
 //# sourceMappingURL=logger.js.map
--- week5/Typescript/src/logger.ts
+++ week5/Typescript/src/logger.ts
@@ -5,7 +5,7 @@ const logger = (a: number, b: string) => {
 let a = 12;
 let b = "Hello World";
 
-logger(a, b);
+// logger(a, b);
 
 interface IPerson {
   name: string;
@@ -20,4 +20,40 @@ const loggerV2 = (a: IPerson, b: IAddress) => {
 
 const person: IPerson = { name: "Kurt" };
 const address: IAddress = { address: "123 Main St" };
-loggerV2(person, address);
+// loggerV2(person, address);
+
+class Person implements IPerson {
+  constructor(private _name: string) {}
+  get name() {
+    return this._name;
+  }
+  set name(val: string) {
+    this._name = val;
+  }
+}
+
+let p = new Person("Kurt Wonnegut");
+
+p.name = "Hanne Wonnegut";
+
+// loggerV2(p, address);
+
+const loggerV3 = <T, U>(a: T, b: U) => {
+  console.log(`Person: ${JSON.stringify(a)}, 
+  Address: ${JSON.stringify(b)}`);
+};
+
+// loggerV3(a, b);
+
+class GenericLogger<T, U> {
+  log = (a: T, b: U) =>
+    console.log(`Val1: ${JSON.stringify(a)}, 
+  Val2: ${JSON.stringify(b)}`);
+}
+
+const logger1 = new GenericLogger<number, string>();
+
+const logger2 = new GenericLogger<IPerson, IAddress>();
+
+logger1.log(a, b);
+logger2.log(person, address);
