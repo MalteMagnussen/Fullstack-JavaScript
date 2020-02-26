b4e50a8df3c73b077d678d6214b24f38162ecb07
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-02-26 11:00:51 +0100

    TabNine used 95% of my CPU. Not worth it.

--- week5/Typescript/build/logger.js
+++ week5/Typescript/build/logger.js
@@ -5,4 +5,10 @@ const logger = (a, b) => {
 let a = 12;
 let b = "Hello World";
 logger(a, b);
+const loggerV2 = (a, b) => {
+    console.log(`Name of Person: ${JSON.stringify(a)}, Address: ${b.address}`);
+};
+const person = { name: "Kurt" };
+const address = { address: "123 Main St" };
+loggerV2(person, address);
 //# sourceMappingURL=logger.js.map
--- week5/Typescript/src/logger.ts
+++ week5/Typescript/src/logger.ts
@@ -6,3 +6,18 @@ let a = 12;
 let b = "Hello World";
 
 logger(a, b);
+
+interface IPerson {
+  name: string;
+}
+interface IAddress {
+  address: string;
+}
+
+const loggerV2 = (a: IPerson, b: IAddress) => {
+  console.log(`Name of Person: ${JSON.stringify(a)}, Address: ${b.address}`);
+};
+
+const person: IPerson = { name: "Kurt" };
+const address: IAddress = { address: "123 Main St" };
+loggerV2(person, address);
