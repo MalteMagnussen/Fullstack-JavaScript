13507265c67bb5a7aeb2bb6b8346e9d8f6cd72f3
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-04 09:55:31 +0100

    Formatting

--- express-mongo-typescript-start-code-master/src/app.ts
+++ express-mongo-typescript-start-code-master/src/app.ts
@@ -1,16 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 import express from "express";
 import path from "path";
 const app = express();
 
-
 app.get("/api/dummy", (req, res) => {
-  res.json({ msg: "Hello" })
-})
+  res.json({ msg: "Hello" });
+});
 
 const PORT = process.env.PORT || 3333;
-const server = app.listen(PORT)
-console.log(`Server started, listening on port: ${PORT}`)
+const server = app.listen(PORT);
+console.log(`Server started, listening on port: ${PORT}`);
 module.exports.server = server;
-
-
