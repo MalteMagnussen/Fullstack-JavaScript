1f2cc2c98b7b01973b0631db2056ab07eb0d7f5a
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-04 17:48:48 +0100

    Implemented Password Hashing

--- express-mongo-typescript-start-code-master/facades/userFacade.ts
+++ express-mongo-typescript-start-code-master/facades/userFacade.ts
@@ -11,7 +11,18 @@ const users: Array<IGameUser> = [];
 class UserFacade {
   static addUser(user: IGameUser): boolean {
     /*Info: Import bcrypt and (npm install bcrypt) and hash before you store */
-    throw Error("Not Implemented");
+    const saltLength = 10;
+    let myBoolean = false;
+    bcrypt.hash(user.password, saltLength, function(err, hash) {
+      if (err) {
+        // TODO HANDLE ERROR
+      } else {
+        user.password = hash;
+        users.push(user);
+        myBoolean = true;
+      }
+    });
+    return myBoolean;
   }
   static deleteUser(userName: string): boolean {
     const index = users.findIndex(user => {
@@ -32,6 +43,19 @@ class UserFacade {
   }
   static checkUser(userName: string, password: string): boolean {
     /*Use bcrypts compare method */
-    throw new Error("Not yet implemented");
+    // Load hash from your password DB.
+    let myBoolean = false;
+    bcrypt.compare(password, this.getUser(userName).password, function(
+      err,
+      res
+    ) {
+      if (err) {
+        // TODO HANDLE ERROR
+      } else {
+        // res === true
+        myBoolean = res;
+      }
+    });
+    return myBoolean;
   }
 }
