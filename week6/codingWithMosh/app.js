0066829f87609698b88904b11b35de88fc0a46a5
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-04 19:34:19 +0100

    Code With Mosh up to Nodemon

--- week6/codingWithMosh/app.js
+++ week6/codingWithMosh/app.js
@@ -5,3 +5,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.get(`/api/courses`, (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
