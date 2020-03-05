8f412ba2ed050e0d2b355f49cce4d92f7875936e
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-05 09:08:37 +0100

    async await

--- express-mongo-typescript-start-code-master/facades/userFacade.ts
+++ express-mongo-typescript-start-code-master/facades/userFacade.ts
@@ -11,21 +11,22 @@ export const users: Array<IGameUser> = [];
 export class UserFacade {
   static addUser(user: IGameUser): boolean {
     /*Info: Import bcrypt and (npm install bcrypt) and hash before you store */
-    const saltLength = 10;
+    const saltRounds = 10;
     let myBoolean = false;
-    // TODO WRAP IN PROMISE
-    bcrypt.hash(user.password, saltLength, (err, hash) => {
-      if (err) {
-        // TODO HANDLE ERROR
-        // REJECT ERROR
-      } else {
-        user.password = hash;
-        users.push(user);
-        myBoolean = true;
-      }
-    });
+    async () => {
+      await bcrypt.hash(user.password, saltRounds, (err, hash) => {
+        if (err) {
+          // TODO HANDLE ERROR WITH DEBUG
+        } else {
+          user.password = hash;
+          users.push(user);
+          myBoolean = true;
+        }
+      });
+    };
     return myBoolean;
   }
+
   static deleteUser(userName: string): boolean {
     const index = users.findIndex(user => {
       return user.name === userName;
@@ -41,23 +42,30 @@ export class UserFacade {
     return users;
   }
   static getUser(userName: string): IGameUser {
-    return users.find(user => user.name === userName);
+    const user = users.find(user => user.name === userName);
+    if (user) return user;
+    else throw new Error("Could not find user");
   }
   static checkUser(userName: string, password: string): boolean {
     /*Use bcrypts compare method */
     // Load hash from your password DB.
 
     let myBoolean = false;
-    // TODO WRAP IN PROMISE
-    bcrypt.compare(password, this.getUser(userName).password, (err, res) => {
-      if (err) {
-        // TODO HANDLE ERROR
-        // TODO REJECT ERROR
-      } else {
-        // res === true
-        myBoolean = res;
-      }
-    });
+
+    async () => {
+      await bcrypt.compare(
+        password,
+        this.getUser(userName).password,
+        (err, res) => {
+          if (err) {
+            // TODO HANDLE ERROR WITH DEBUG
+          } else {
+            // res === true
+            myBoolean = res;
+          }
+        }
+      );
+    };
     return myBoolean;
   }
 }
