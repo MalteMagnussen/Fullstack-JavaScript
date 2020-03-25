7ed967b21b2ba3802fb6ab717fc7061a20f7bc02
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-03-25 11:06:56 +0100

    initial

---
+++ week9/GEO-JSON/app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const gju = require("geojson-utils");
+const { gameArea, players } = require("./gameData");
+app.get("/", (req, res) => res.send("Geo Demo!"));
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
