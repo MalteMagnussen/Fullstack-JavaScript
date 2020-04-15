e65f78976fb3eac52353e4ba5203468efb0c1bb8
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-15 13:30:07 +0200

    Update App.js

--- week11/goal/App.js
+++ week11/goal/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <TextInput
+          placeholder="Make a goal"
+          style={{
+            borderBottomColor: "black",
+            borderWidth: 1,
+            padding: 10
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
