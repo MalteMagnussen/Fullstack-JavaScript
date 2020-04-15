551d1ca38ebd20d82d5bf3ca1ec1c93969c8d062
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-15 18:28:06 +0200

    Formatting

--- week11/reactnative-many-demos-master/examples/props.js
+++ week11/reactnative-many-demos-master/examples/props.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 Cafe = () => {
   return (
-    <View style={{  flex: 1, paddingTop: 22}}>
-    <Text style={{fontSize: 18}}>Change me to demonstrate basics of React Props</Text>
+    <View style={{ flex: 1, paddingTop: 22 }}>
+      <Text style={{ fontSize: 18 }}>
+        Change me to demonstrate basics of React Props
+      </Text>
     </View>
   );
-}
+};
 
 //https://reactnative.dev/docs/intro-react#props
 export default function PropsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{fontSize:24}}>PropsDemo</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 24 }}>PropsDemo</Text>
       <Cafe />
     </View>
   );
 }
-
