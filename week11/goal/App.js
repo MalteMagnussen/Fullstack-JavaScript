a189ba1a75d6b63e9c07906e6e6539de6934dda3
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-15 13:32:55 +0200

    Update App.js

--- week11/goal/App.js
+++ week11/goal/App.js
@@ -4,13 +4,20 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 export default function App() {
   return (
     <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <TextInput
           placeholder="Make a goal"
           style={{
             borderBottomColor: "black",
             borderWidth: 1,
-            padding: 10
+            padding: 10,
+            width: "80%",
           }}
         />
         <Button title="ADD" />
