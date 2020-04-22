bc37c301d52018affcb7fa823ad698c2d92de18e
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-22 11:43:16 +0200

    fetch call

--- week12/geoDemoNoDB-Client-startcode-master/serverFacade.js
+++ week12/geoDemoNoDB-Client-startcode-master/serverFacade.js
@@ -1,23 +1,34 @@
-
 import { SERVER_URL } from "./settings";
 
 ServerFacade = () => {
-
   async function fetchGameArea() {
-    const res = await fetch(`${SERVER_URL}/geoapi/gamearea`).then(res => res.json());
+    const res = await fetch(`${SERVER_URL}/geoapi/gamearea`).then((res) =>
+      res.json()
+    );
     return res.coordinates;
   }
 
   async function isUserInArea(lon, lat) {
-    const status = await fetch(`${SERVER_URL}/geoapi/isuserinarea/${lon}/${lat}`).
-                    then(res => res.json())
+    const status = await fetch(
+      `${SERVER_URL}/geoapi/isuserinarea/${lon}/${lat}`
+    ).then((res) => res.json());
+    return status;
+  }
+
+  // {"userName":"team1", "password":"secret", "lat":3, "lon": 5,"distance": 3}
+  async function findNearbyPlayers(userName, password, lat, lon, distance) {
+    const status = await fetch(`${SERVER_URL}/gameapi/nearbyplayers`, {
+      method: "post",
+      body: JSON.stringify({ userName, password, lat, lon, distance }),
+    }).then((res) => res.json());
     return status;
   }
 
   return {
     fetchGameArea,
-    isUserInArea
-  }
-}
+    isUserInArea,
+    findNearbyPlayers,
+  };
+};
 
 export default ServerFacade();
