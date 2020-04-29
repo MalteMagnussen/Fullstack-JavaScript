7f149ea4c52a0e2aab56e4d45913ecd04763743a
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-29 15:49:10 +0200

    First Video

--- week13/lynda-video-mongodb-only-master/index.js
+++ week13/lynda-video-mongodb-only-master/index.js
@@ -1,20 +1,23 @@
 import express from "express";
-import graphqlHTTP from "express-graphql"
+import graphqlHTTP from "express-graphql";
 //import schema from "./schemaOriginal"
-import {schema} from "./data/schema"
+import { schema } from "./data/schema";
 //import resolvers from "./resolversOriginal"
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("GraphQL is amazing")
-})
+  res.send("GraphQL is amazing!");
+});
 
 //const root = resolvers;
 
-app.use("/graphql", graphqlHTTP({
-  schema: schema,
-  //rootValue: root,
-  graphiql: true
-}))
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    //rootValue: root,
+    graphiql: true,
+  })
+);
 
-app.listen(8080, () => console.log("Server Started, listening on Port 8080"))
+app.listen(8080, () => console.log("Running server on port localhost:8080"));
