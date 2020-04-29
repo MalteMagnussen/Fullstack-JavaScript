f18d4b87ed4b4bd7a48ddb00c0ecbeaffafd4050
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-04-29 15:48:31 +0200

    GraphQL is amazing

---
+++ week13/lynda-video-mongodb-only-master/data/dbConnectors.js
@@ -0,0 +1,22 @@
+require("dotenv").config({ path: path.join(process.cwd(), ".env") });
+import mongoose from "mongoose";
+
+const CONNECTION = process.env.CONNECTION; //"ADD YOUR OWN CONNECTION STRING"
+
+mongoose.connect(CONNECTION, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const friendSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  gender: { type: String },
+  age: { type: Number },
+  language: { type: String },
+  email: { type: String },
+  contacts: { type: Array },
+});
+
+const Friends = mongoose.model("Friend", friendSchema);
+export { Friends };
